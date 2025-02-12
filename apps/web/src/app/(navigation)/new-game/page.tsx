ac6c719161d1798b512f317936c4802c99e38852
etan094 <etan094@gmail.com>
2025-02-11 19:50:09 -0800

    new game page

--- apps/web/src/app/(navigation)/new-game/page.tsx
+++ apps/web/src/app/(navigation)/new-game/page.tsx
@@ -1,12 +1,104 @@
+"use client";
+
+import GameTable from "./components/GameTable";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 export default function NewGamePage() {
+  const [name, setName] = useState("");
+  const [buyIns, setBuyIns] = useState<number>(0);
+  const [gainsLosses, setGainsLosses] = useState<number>(0);
+  const [players, setPlayers] = useState<
+    { name: string; buyIns: number; gainsLosses: number }[]
+  >([]);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setPlayers([...players, { name, buyIns, gainsLosses }]);
+    setName("");
+    setBuyIns(0);
+    setGainsLosses(0);
+
+    localStorage.setItem(
+      "players",
+      JSON.stringify([...players, { name, buyIns, gainsLosses }])
+    );
+  };
+
+  const handleDelete = (index: number) => {
+    const newPlayers = players.filter((player, i) => i !== index);
+    setPlayers(newPlayers);
+    localStorage.setItem("players", JSON.stringify(newPlayers));
+  };
+
+  useEffect(() => {
+    const playersLocalStorage = localStorage.getItem("players");
+    if (playersLocalStorage) {
+      setPlayers(JSON.parse(playersLocalStorage));
+    }
+  }, []);
+
+  const totalBuyIns = players.reduce((acc, player) => acc + player.buyIns, 0);
+
   return (
     <div>
-      <h1>New Game</h1>
+      <h1 className=" my-8">New Game</h1>
 
       <div>
-        <Input type="text" placeholder="enter player name" />
+        <form>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <label htmlFor="name" className="text-right">
+                Name
+              </label>
+              <Input
+                id="name"
+                className="col-span-3"
+                name="name"
+                required
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <label htmlFor="buyIns">Buy Ins</label>
+              <Input
+                id="buyIns"
+                type="number"
+                name="buyIns"
+                required
+                className="col-span-3"
+                onChange={(e) => setBuyIns(Number(e.target.value || undefined))}
+                value={buyIns}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <label htmlFor="gainslosses">Gains/Losses</label>
+              <Input
+                id="gainslosses"
+                type="number"
+                name="gainslosses"
+                required
+                className="col-span-3"
+                onChange={(e) =>
+                  setGainsLosses(Number(e.target.value || undefined))
+                }
+                value={gainsLosses}
+              />
+            </div>
+          </div>
+          <Button variant="default" onClick={(e) => handleSubmit(e)}>
+            Submit
+          </Button>
+        </form>
+
+        <div>
+          <GameTable players={players} handleDelete={handleDelete} />
+        </div>
+        <div className=" border-t mt-4 pt-4">
+          <p>Total Buy Ins: ${totalBuyIns}</p>
+        </div>
       </div>
     </div>
   );
