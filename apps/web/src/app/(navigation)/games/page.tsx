6d8be9fbdd6be3abb8bf84a06a96d48551e18e39
etan094 <etan094@gmail.com>
2025-02-24 15:02:25 -0800

    Added image on the front page and moved game button

--- apps/web/src/app/(navigation)/games/page.tsx
+++ apps/web/src/app/(navigation)/games/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default async function GamePage() {
   return (
-    <div>
+    <div className=" flex justify-center mt-9">
       <Button>
         <Link href={"/games/new-game"}>Start New Game</Link>
       </Button>
